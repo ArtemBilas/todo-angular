67ead9badd2fa90fdf05cbfbe0e7269a38583300
Artem Bilas <artembilas@gmail.com>
2023-02-02 15:19:45 +0200

    fix: delete shared input component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrimengModule } from '../primeng/primeng.module';
-import { InputComponent } from './components/input/input.component';
-
-const sharedComponents = [
-  InputComponent
-  ];
 
 @NgModule({
-  declarations: [
-    ...sharedComponents
-    ],
+  declarations: [],
   imports: [
     CommonModule,
     PrimengModule
     ],
   exports: [
     PrimengModule,
-    ...sharedComponents
     ]
 })
 export class SharedModule { }
